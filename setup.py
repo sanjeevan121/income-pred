78fc6c387b795eccb9f6de1f70fe548e0f779a5c
sanjeevan121 <sanjeevan.k.thorat@gmail.com>
2021-10-18 15:22:57 +0530

    update schemas for deployment

--- setup.py
+++ setup.py
@@ -7,5 +7,4 @@ setup(
     description='income prediction',
     author='sanjeevan thorat',
     license='MIT',
-    dependency_links=['https://github.com/sanjeevan121/income-pred.git@246cdf41359cf4d0982ecb2ed6a6e82a572bf58b#egg=src']
 )
